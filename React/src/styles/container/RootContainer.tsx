fae699a0a8b0ea883a3a163eedd0a8b0ca62f7ed
owen <wavefunctionc@gmail.com>
2024-01-17 22:15:00 +0900

    Root Container & Main Tab 컴포넌트 추가

--- React/src/styles/container/RootContainer.tsx
+++ React/src/styles/container/RootContainer.tsx
@@ -1,5 +1,9 @@
+import {Container} from "react-bootstrap";
 import styled from "styled-components";
 
-export const RootContainer = styled.div`
-    
+export const RootContainer = styled(Container)`
+    font-family: Arial, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    align-items: center;
 `;
