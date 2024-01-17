9bdec3ac5ab70a5e3d0468dde97f981742624d4a
owen <wavefunctionc@gmail.com>
2024-01-17 23:23:57 +0900

    Root Container margin 0, max-width 제한

--- React/src/styles/container/RootContainer.tsx
+++ React/src/styles/container/RootContainer.tsx
@@ -5,5 +5,10 @@ export const RootContainer = styled(Container)`
     font-family: Arial, sans-serif;
     font-size: 15px;
     font-weight: bold;
-    align-items: center;
+    background-image: url("../../../public/assets/background/earth.jpg");
+    background-size: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
 `;
