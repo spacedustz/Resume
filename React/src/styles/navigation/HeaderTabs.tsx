fd08b725f7457320db712cd92f9e6b67bbff7664
spacedustz <tensorflow555@gmail.com>
2024-01-17 23:23:15 +0900

    Tab Hover Style 변경

--- React/src/styles/navigation/HeaderTabs.tsx
+++ React/src/styles/navigation/HeaderTabs.tsx
@@ -3,10 +3,24 @@ import Tabs from 'react-bootstrap/Tabs';
 import React from "react";
 import Introduce from "../../components/Introduce.tsx";
 import SkillList from "../card/SkillList.tsx";
+import styled from "styled-components";
+
+const TabsStyle = styled(Tabs)`
+    button {
+        color: white;
+        
+        &:hover {
+            background-color: dimgray;
+            color: white;
+            transform: scale(1.12);
+            transition: color 0.5s;
+        }
+    }
+`;
 
 const HeaderTabs: React.FC = () => {
     return (
-        <Tabs
+        <TabsStyle
             defaultActiveKey="profile"
             id="justify-tab-example"
             className="mb-3"
@@ -31,7 +45,7 @@ const HeaderTabs: React.FC = () => {
             <Tab eventKey="educations" title="Educations">
                 Tab content for Contact
             </Tab>
-        </Tabs>
+        </TabsStyle>
     );
 }
 
