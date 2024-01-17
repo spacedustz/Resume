fffb5d69eea2d60484862fae5ab6faf52f0cedfa
owen <floralexium@gmail.com>
2024-01-17 17:52:18 +0900

    Add React Bootstrap & Styled Components

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navigation from "./styles/navigation/Navigation.tsx";
+import HeaderTabs from "./styles/navigation/HeaderTabs.tsx";
 const App: React.FC = () => {
 
   return (
     <>
+      <Navigation />
+      <HeaderTabs />
     </>
   )
 }
