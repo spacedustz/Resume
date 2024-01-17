a61582dbc3ea8b3d20230e1cf1ad55217513596c
Kunwoo Shin <recyclefunction@gmail.com>
2024-01-17 23:23:02 +0900

    Root에 Navigation 추가

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import HeaderTabs from "./styles/navigation/HeaderTabs.tsx";
 import ProfileCard from "./styles/card/ProfileCard.tsx";
 import {RootContainer} from "./styles/container/RootContainer.tsx";
+import Navigation from "./styles/navigation/Navigation.tsx";
 
 const App: React.FC = () => {
 
     return (
         <RootContainer>
+            <Navigation />
             <ProfileCard/>
             <HeaderTabs/>
         </RootContainer>
