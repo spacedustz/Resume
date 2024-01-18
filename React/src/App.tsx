ff6530fb06e5a6c58e89505136e083e589da5fe9
spacedustz <tensorflow555@gmail.com>
2024-01-18 17:17:39 +0900

    Router 모듈 위치 변경

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProfileCard from "./styles/card/ProfileCard.tsx";
 import {RootContainer} from "./styles/container/RootContainer.tsx";
 import Navigation from "./styles/navigation/Navigation.tsx";
-import Router from "./components/Router.tsx";
+import Router from "./router/Router.tsx";
 
 const App: React.FC = () => {
 
