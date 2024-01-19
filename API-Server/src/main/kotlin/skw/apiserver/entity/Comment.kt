216ac5675a11a0d46215b06d77e0ec64938f6659
spacedustz <tensorflow555@gmail.com>
2024-01-19 19:23:15 +0900

    Kotlin DSL 수정

---
+++ API-Server/src/main/kotlin/skw/apiserver/entity/Comment.kt
@@ -0,0 +1,26 @@
+package skw.apiserver.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Entity
+data class Comment(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    var comment: String? = null,
+    var createdAt: String? = null
+) {
+    constructor(comment: String, createdAt: String) : this(null, comment, createdAt)
+
+    companion object {
+        fun createOf(comment: String): Comment {
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH시 mm분 ss초")
+            return Comment(comment, LocalDateTime.now().format(formatter).toString())
+        }
+    }
+}
