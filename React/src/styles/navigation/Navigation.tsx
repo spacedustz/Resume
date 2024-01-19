f4852aea9064467a0ff376593465e8ae39b9fb0a
owen <wavefunctionc@gmail.com>
2024-01-19 15:22:22 +0900

    Navigation Animation 적용

--- React/src/styles/navigation/Navigation.tsx
+++ React/src/styles/navigation/Navigation.tsx
@@ -2,17 +2,34 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const colorChange = keyframes`
+    0% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.5);
+    }
+    50% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.8);
+    }
+    100% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.5);
+    }
+`;
 
 const NavStyle = styled(Navbar)`
     a {
         margin: 2px;
+        animation: ${colorChange} 3s infinite;
         
         &:hover {
             background-color: rgba(250, 250, 210, 0.5);
             color: rgba(250, 250, 210, 0.5);
             transform: scale(1.12);
             transition: color 0.5s;
+            animation: none;
         }
     }
 `;
