670e24fb28270d2634872a719b21b95c4dd4407d
spacedustz <tensorflow555@gmail.com>
2024-01-19 19:52:35 +0900

    Comment 기능 테스트

--- API-Server/src/main/kotlin/skw/apiserver/entity/Comment.kt
+++ API-Server/src/main/kotlin/skw/apiserver/entity/Comment.kt
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import lombok.NoArgsConstructor
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Entity
+@NoArgsConstructor
 data class Comment(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
