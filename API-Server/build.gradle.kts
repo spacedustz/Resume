44962964b34d33444defa5ef0f8f0890beeaaa93
owen <floralexium@gmail.com>
2024-01-21 02:20:51 +0900

    Jwt 추가

--- API-Server/build.gradle.kts
+++ API-Server/build.gradle.kts
@@ -26,14 +26,32 @@ repositories {
 }
 
 dependencies {
+	// Web
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// JPA
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// Security
 	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// WebSocket
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+
+	// Webflux
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	compileOnly("org.projectlombok:lombok")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	// MariaDB
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
