eaae34e90fff6e2144ad1b7e3676333e63efbf48
spacedustz <tensorflow555@gmail.com>
2024-01-21 02:53:59 +0900

    User Repo Primary Key 타입 수정

--- API-Server/src/main/kotlin/skw/apiserver/repository/UserRepository.kt
+++ API-Server/src/main/kotlin/skw/apiserver/repository/UserRepository.kt
@@ -3,9 +3,8 @@ package skw.apiserver.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import skw.apiserver.entity.User
 import skw.apiserver.enum.UserType
-import java.util.UUID
 
-interface UserRepository : JpaRepository<User, UUID> {
-    fun findByName(name: String): skw.apiserver.entity.User?
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByName(name: String): User?
     fun findAllByType(type: UserType): List<User>
 }
