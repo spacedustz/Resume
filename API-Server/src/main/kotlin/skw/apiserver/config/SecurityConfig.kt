ed13466d9e52798bc7ed72c445bc61cada1c8b14
owen <wavefunctionc@gmail.com>
2024-01-21 03:16:36 +0900

    Security Config에 Jwt Filter 추가

--- API-Server/src/main/kotlin/skw/apiserver/config/SecurityConfig.kt
+++ API-Server/src/main/kotlin/skw/apiserver/config/SecurityConfig.kt
@@ -3,18 +3,24 @@ package skw.apiserver.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import skw.apiserver.filter.JwtAuthenticationFilter
 
 /**
  * @author 신건우
  * @desc 쿠키, 세션 사용 X, CSRF Disable
  */
 @Configuration
-class SecurityConfig {
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+) {
     private val defaultPages = arrayOf(
         "/",
         "/intro",
@@ -43,6 +49,7 @@ class SecurityConfig {
 //                .anyRequest().authenticated()
         }
         .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter::class.java)
         .build()!!
 
     @Bean
