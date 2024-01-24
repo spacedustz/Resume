5bdb44952d0ea886bac3c4c17a4d9661b438ce03
Kunwoo Shin <recyclefunction@gmail.com>
2024-01-25 00:52:06 +0900

    Cross Origin 전체 허용

--- API-Server/src/main/kotlin/skw/apiserver/config/WebConfig.kt
+++ API-Server/src/main/kotlin/skw/apiserver/config/WebConfig.kt
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://43.202.203.180:3000")
+//            .allowedOrigins("http://43.202.203.180:3000")
+            .allowedOrigins("*")
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
