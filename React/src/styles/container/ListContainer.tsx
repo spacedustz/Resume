2153c8207af5439e5f468ec257ac4afe02c5d8a9
owen <wavefunctionc@gmail.com>
2024-01-26 12:46:45 +0900

    List Container Sub Scroll Style 주석처리

--- React/src/styles/container/ListContainer.tsx
+++ React/src/styles/container/ListContainer.tsx
@@ -13,3 +13,32 @@ export const ListContainer = styled(Container)`
         color: khaki;
     }
 `;
+
+// export const ScrollStyledContainer = styled(ListContainer)`
+//     max-height: 100vh;
+//     max-width: 100vw;
+//     overflow-y: auto;
+//
+//     &::-webkit-scrollbar {
+//         width: 8px;
+//         height: 8px;
+//         border-radius: 6px;
+//         background: rgba(255, 255, 255, 0.4);
+//     }
+//
+//     &::-webkit-scrollbar-track {
+//         background: rgba(33, 122, 244, .1);
+//         border-radius: 10px;
+//     }
+//
+//     &::-webkit-scrollbar-thumb {
+//         height: 30%;
+//         background: rgba(0, 0, 0, 0.3);
+//         border-radius: 6px;
+//         transition: background 0.3s ease-in-out;
+//     }
+//
+//     &::-webkit-scrollbar-thumb:hover {
+//         background: #888;
+//     }
+// `;
