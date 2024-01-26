e6f6ab41d3e2aa4900addf77f2f9aead90bd6229
owen <floralexium@gmail.com>
2024-01-26 10:38:18 +0900

    Root Container Base Tag 변경

--- React/src/styles/container/RootContainer.tsx
+++ React/src/styles/container/RootContainer.tsx
@@ -1,14 +1,12 @@
-import {Container} from "react-bootstrap";
 import styled from "styled-components";
 
-export const RootContainer = styled(Container)`
+export const RootContainer = styled.div`
     font-family: Arial, sans-serif;
     font-size: 15px;
     font-weight: bold;
     background-image: url("../../../public/assets/background/earth.jpg");
     background-size: 100%;
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 2rem;
+    width: 100vw;
+    height: 100vh;
     text-align: center;
 `;
