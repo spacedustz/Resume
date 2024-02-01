4fdf6e216a3392b7d5cf7376414a13c4b829f7c2
owen <wavefunctionc@gmail.com>
2024-02-01 09:47:17 +0900

    프로필 카드 사진 다시 올림

--- React/src/styles/card/ProfileCard.tsx
+++ React/src/styles/card/ProfileCard.tsx
@@ -62,7 +62,7 @@ const ProfileCard: React.FC = () => {
     return (
         <CardContainer>
             <MainCard style={{width: '20rem'}}>
-                {/*<Card.Img variant="top" src="../../public/assets/me2.png"/>*/}
+                <Card.Img variant="top" src="../../public/assets/me2.png"/>
                 <Card.Body>
                         <Title>😊 안녕하세요 😊</Title><br/>
                         <strong>개발자 신건우입니다.</strong><br/>
