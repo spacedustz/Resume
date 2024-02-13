23944b0f80c9ad141aa0a36f346087ad927c0ba7
owen <floralexium@gmail.com>
2024-02-13 16:33:57 +0900

    Tech Stack Routing URL 변경

--- React/src/router/Router.tsx
+++ React/src/router/Router.tsx
@@ -1,13 +1,13 @@
 import React, {Suspense} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Introduce from "../components/Introduce.tsx";
-import SkillList from "../components/Skills.tsx";
 import Careers from "../components/Careers.tsx";
 import Licenses from "../components/Licenses.tsx";
 import Educations from "../components/Educations.tsx";
 import HeaderTabs from "../components/HeaderTabs.tsx";
 import Navigation from "../components/Navigation.tsx";
 import ProfileCard from "../components/ProfileCard.tsx";
+import TechStacks from "../components/TechStacks.tsx";
 
 const Router: React.FC = () => {
     return (
@@ -19,7 +19,7 @@ const Router: React.FC = () => {
                     <Routes>
                         <Route path="/*" element={<HeaderTabs/>}>
                             <Route path="intro" element={<Introduce/>}/>
-                            <Route path="skills" element={<SkillList/>}/>
+                            <Route path="techstack" element={<TechStacks />}/>
                             <Route path="careers" element={<Careers/>}/>
                             <Route path="licenses" element={<Licenses/>}/>
                             <Route path="educations" element={<Educations/>}/>
