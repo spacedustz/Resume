c4c26e6a71c6eef58df0b98dce3e2272ae027ef5
owen <floralexium@gmail.com>
2024-02-13 16:00:37 +0900

    프로필 이미지 변경 및 스타일 변경

--- React/src/components/ProfileCard.tsx
+++ React/src/components/ProfileCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import {CardContainer, MainCard, Title} from "../styles/card/ProfileCard.ts";
+import {CardContainer, MainCard} from "../styles/card/ProfileCard.ts";
 
 const ProfileCard: React.FC = () => {
     return (
@@ -8,9 +8,9 @@ const ProfileCard: React.FC = () => {
             <MainCard style={{width: '20rem'}}>
                 <Card.Img variant="top" src="../../public/assets/profile/profile-logo.png"/>
                 <Card.Body>
-                    <Title>😊 안녕하세요 😊</Title><br/>
-                    <strong>개발자 신건우입니다.</strong><br/>
-                    <strong>spacedustw@gmail.com</strong>
+                    <strong>😊 안녕하세요 😊</strong><br/>
+                    <p>개발자 신건우입니다.</p>
+                    <p>spacedustw@gmail.com</p>
                 </Card.Body>
             </MainCard>
         </CardContainer>
