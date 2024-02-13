313cfbfe8886616595ce525b48716a05974215d5
owen <wavefunctionc@gmail.com>
2024-02-13 15:51:07 +0900

    Text Color 클래스 분리

---
+++ React/src/styles/animation/TextColor.ts
@@ -0,0 +1,31 @@
+import {keyframes} from "styled-components";
+
+export const NavColorChange = keyframes`
+    0% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(177, 50, 170, 0.84);
+    }
+    50% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(70, 100, 210, 0.72);
+    }
+    100% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(130, 130, 210, 1);
+    }
+`;
+
+export const ColorChange = keyframes`
+    0% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.5);
+    }
+    50% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.8);
+    }
+    100% {
+        background-color: rgba(250, 250, 210, 0);
+        color: rgba(250, 250, 210, 0.5);
+    }
+`;
