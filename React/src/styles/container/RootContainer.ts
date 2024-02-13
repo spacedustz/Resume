a6dcb7523649f91afa95e263e12a5eb9074e3f5c
spacedustz <tensorflow555@gmail.com>
2024-02-13 15:30:46 +0900

    Rename & Move Components or .ts Files

---
+++ React/src/styles/container/RootContainer.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const RootContainer = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+    background-image: url("../../../public/assets/background/night.jpg");
+    background-size: 100% 100vh;
+    background-position: center top;
+    background-repeat: repeat-y;
+    min-height: 100vh;
+    max-width: 100vw;
+    text-align: center;
+`;
