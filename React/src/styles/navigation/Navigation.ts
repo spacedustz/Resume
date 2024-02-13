f13aad9310b2a0721018cddc14d1a5596d12bbad
owen <wavefunctionc@gmail.com>
2024-02-13 15:51:42 +0900

    Navigation Media Query 적용

--- React/src/styles/navigation/Navigation.ts
+++ React/src/styles/navigation/Navigation.ts
@@ -1,32 +1,27 @@
 import Navbar from 'react-bootstrap/Navbar';
-import styled, {keyframes} from "styled-components";
-
-export const colorChange = keyframes`
-    0% {
-        background-color: rgba(250, 250, 210, 0);
-        color: rgba(250, 250, 210, 0.5);
-    }
-    50% {
-        background-color: rgba(250, 250, 210, 0);
-        color: rgba(250, 250, 210, 0.8);
-    }
-    100% {
-        background-color: rgba(250, 250, 210, 0);
-        color: rgba(250, 250, 210, 0.5);
-    }
-`;
+import styled from "styled-components";
+import {NavColorChange} from "../animation/TextColor.ts";
 
 export const NavStyle = styled(Navbar)`
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    padding-right: 30px;
+    
     a {
         margin: 2px;
-        animation: ${colorChange} 3s infinite;
+        animation: ${NavColorChange} 3s infinite;
         
         &:hover {
-            background-color: rgba(250, 250, 210, 0.5);
+            background-color: rgba(250, 250, 210, 0);
             color: rgba(250, 250, 210, 0.5);
-            transform: scale(1.12);
+            transform: scale(1.05);
             transition: color 0.5s;
             animation: none;
+        }
+
+        @media (max-width: 768px) {
+            font-size: 10px;
         }
     }
 `;
