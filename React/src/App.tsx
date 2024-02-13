7124f676e2791019f214fdf62bced42ab72f3ff1
owen <wavefunctionc@gmail.com>
2024-02-13 15:28:53 +0900

    App 하위 컴포넌트 Router로 이동

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import ProfileCard from "./styles/card/ProfileCard.tsx";
-import {RootContainer} from "./styles/container/RootContainer.tsx";
-import Navigation from "./styles/navigation/Navigation.tsx";
+import {RootContainer} from "./styles/container/RootContainer.ts";
 import Router from "./router/Router.tsx";
 
 const App: React.FC = () => {
 
     return (
         <RootContainer>
-            <Navigation />
-            <ProfileCard/>
             <Router />
         </RootContainer>
     )
